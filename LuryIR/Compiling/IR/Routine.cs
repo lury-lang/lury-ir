b0acdbefbf2f7ad4f3fee5042684a412154c3e8d
Tomona Nanase <nanase@users.noreply.github.com>
2015-10-17 03:51:06 +0900

    Add Routine#Dump

--- LuryIR/Compiling/IR/Routine.cs
+++ LuryIR/Compiling/IR/Routine.cs
@@ -28,6 +28,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 using Lury.Compiling.Utils;
 
 namespace Lury.Compiling.IR
@@ -73,7 +75,7 @@ namespace Lury.Compiling.IR
 
             if (allocSize < 0)
                 throw new ArgumentOutOfRangeException("allocSize");
-            
+
             this.Name = name;
             this.AllocSize = allocSize;
             this.children = (IReadOnlyList<Routine>)children ?? new Routine[0];
@@ -96,7 +98,119 @@ namespace Lury.Compiling.IR
 
         #region -- Public Methods --
 
-        
+        public string Dump()
+        {
+            StringBuilder sb = new StringBuilder();
+            int positionWidth = GetPositionWidth(this);
+
+            this.DumpPrivate(sb, 0, positionWidth);
+
+            return sb.ToString();
+        }
+
+        #endregion
+
+        #region -- Private Methods --
+
+        private void DumpPrivate(StringBuilder sb, int indent, int positionWidth)
+        {
+            const int indentWidth = 4;
+
+            this.DumpPosition(sb, positionWidth, -1);
+            sb.Append(' ', indentWidth * indent);
+            sb.Append("::");
+            sb.AppendLine(this.Name);
+            indent++;
+
+            sb.Append(' ', positionWidth + indentWidth * indent);
+            sb.AppendFormat("# alloc {0} register", this.AllocSize);
+            sb.AppendLine();
+
+            foreach (var child in this.children)
+                child.DumpPrivate(sb, indent, positionWidth);
+
+            var labels = this.jumpLabels.ToDictionary(k => k.Value, v => v.Key);
+
+            for (int i = 0; i < this.instructions.Count; i++)
+            {
+                if (labels.ContainsKey(i))
+                {
+                    sb.Append(' ', positionWidth + indentWidth * (indent - 1));
+                    sb.Append(":");
+                    sb.AppendLine(labels[i]);
+                }
+
+                this.DumpPosition(sb, positionWidth, i);
+                
+                var inst = this.instructions[i];
+
+                sb.Append(' ', indentWidth * indent);
+                if (inst.Destination != Instruction.NoAssign)
+                {
+                    sb.Append('%');
+                    sb.Append(inst.Destination);
+                    sb.Append(" = ");
+                }
+                sb.Append(inst.Operation.ToString().ToLower());
+                
+                if (inst.Parameters.Count > 0)
+                {
+                    sb.Append(' ');
+                    sb.Append(string.Join(" ", inst.Parameters));
+                }
+                sb.AppendLine();
+            }
+        }
+
+        private void DumpPosition(StringBuilder sb, int positionWidth, int line)
+        {
+            if (this.codePosition.ContainsKey(line))
+            {
+                var pos = this.codePosition[line];
+                var str = string.Format(
+                    "L{0},{1}{2}",
+                    pos.Position.Line,
+                    pos.Position.Column,
+                    pos.Length > 0 ? "(" + pos.Length + ")" : "");
+
+                sb.Append(str);
+                sb.Append(' ', Math.Max(positionWidth - str.Length, 1));
+            }
+            else
+            {
+                sb.Append(' ', positionWidth);
+            }
+        }
+
+        #endregion
+
+        #region -- Private Static Methods --
+
+        private static int GetPositionWidth(Routine routine)
+        {
+            int res;
+            if (routine.codePosition.Count > 0)
+            {
+                var p = routine.codePosition.Select(k => k.Value).ToArray();
+                int lmax = Math.Max((int)Math.Log10(p.Max(c => c.Length)), 0);
+                int cmax = (int)Math.Log10(p.Max(c => c.Position.Column));
+                int rmax = (int)Math.Log10(p.Max(c => c.Position.Line));
+                res = lmax + cmax + rmax + 3 + 6;
+            }
+            else
+                res = 0;
+
+            foreach (var child in routine.children)
+            {
+                int cres = GetPositionWidth(child);
+
+                if (res < cres)
+                    res = cres;
+            }
+
+            return res;
+        }
+
 
         #endregion
     }
