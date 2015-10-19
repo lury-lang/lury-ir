fd718a551e2edbcfae6948444a896e1117938ccc
Tomona Nanase <nanase@users.noreply.github.com>
2015-10-19 18:11:04 +0900

    Add XML Documents at Parameter

--- LuryIR/Compiling/IR/Parameter.cs
+++ LuryIR/Compiling/IR/Parameter.cs
@@ -32,22 +32,40 @@ using System.Text;
 
 namespace Lury.Compiling.IR
 {
+    /// <summary>
+    /// 操作に用いられるパラメータをカプセル化します。
+    /// </summary>
     public class Parameter
     {
         #region -- Public Fields --
 
+        /// <summary>
+        /// nil を表す Parameter オブジェクトです。この変数は読み取り専用です。
+        /// </summary>
         public readonly static Parameter Nil = new Parameter(null, ParameterType.Nil);
 
+        /// <summary>
+        /// true を表す Parameter オブジェクトです。この変数は読み取り専用です。
+        /// </summary>
         public readonly static Parameter True = new Parameter(true, ParameterType.Boolean);
 
+        /// <summary>
+        /// false を表す Parameter オブジェクトです。この変数は読み取り専用です。
+        /// </summary>
         public readonly static Parameter False = new Parameter(false, ParameterType.Boolean);
 
         #endregion
 
         #region -- Public Properties --
 
+        /// <summary>
+        /// パラメータの値を取得します。
+        /// </summary>
         public object Value { get; private set; }
 
+        /// <summary>
+        /// パラメータの種類を表す <see cref="Lury.Compiling.IR.ParameterType"/> の値を取得します。
+        /// </summary>
         public ParameterType Type { get; private set; }
 
         #endregion
@@ -64,6 +82,10 @@ namespace Lury.Compiling.IR
 
         #region -- Public Methods --
 
+        /// <summary>
+        /// このオブジェクトを表す文字列を取得します。
+        /// </summary>
+        /// <returns>オブジェクトの種類とその値を含んだ文字列。</returns>
         public override string ToString()
         {
             switch (this.Type)
@@ -85,26 +107,52 @@ namespace Lury.Compiling.IR
 
         #region -- Public Static Methods --
 
+        /// <summary>
+        /// 整数値から <see cref="Lury.Compiling.IR.Parameter"/> オブジェクトを生成します。
+        /// </summary>
+        /// <param name="value">パラメータとして渡される整数値。</param>
+        /// <returns>生成された <see cref="Lury.Compiling.IR.Parameter"/> オブジェクト。</returns>
         public static Parameter GetInteger(long value)
         {
             return new Parameter(value, ParameterType.Integer);
         }
 
+        /// <summary>
+        /// 実数値から <see cref="Lury.Compiling.IR.Parameter"/> オブジェクトを生成します。
+        /// </summary>
+        /// <param name="value">パラメータとして渡される実数値。</param>
+        /// <returns>生成された <see cref="Lury.Compiling.IR.Parameter"/> オブジェクト。</returns>
         public static Parameter GetReal(double value)
         {
             return new Parameter(value, ParameterType.Real);
         }
 
+        /// <summary>
+        /// 複素数値から <see cref="Lury.Compiling.IR.Parameter"/> オブジェクトを生成します。
+        /// </summary>
+        /// <param name="re">パラメータとして渡される複素数値の実数部分。</param>
+        /// <param name="im">パラメータとして渡される複素数値の虚数部分。</param>
+        /// <returns>生成された <see cref="Lury.Compiling.IR.Parameter"/> オブジェクト。</returns>
         public static Parameter GetComplex(double re, double im)
         {
             return new Parameter(new Complex(re, im), ParameterType.Complex);
         }
 
+        /// <summary>
+        /// 真偽値から <see cref="Lury.Compiling.IR.Parameter"/> オブジェクトを生成します。
+        /// </summary>
+        /// <param name="value">パラメータとして渡される真偽値。</param>
+        /// <returns>生成された <see cref="Lury.Compiling.IR.Parameter"/> オブジェクト。</returns>
         public static Parameter GetBoolean(bool value)
         {
             return value ? True : False;
         }
 
+        /// <summary>
+        /// 文字列から <see cref="Lury.Compiling.IR.Parameter"/> オブジェクトを生成します。
+        /// </summary>
+        /// <param name="value">パラメータとして渡される文字列。</param>
+        /// <returns>生成された <see cref="Lury.Compiling.IR.Parameter"/> オブジェクト。</returns>
         public static Parameter GetString(string value)
         {
             if (value == null)
@@ -113,16 +161,33 @@ namespace Lury.Compiling.IR
             return new Parameter(value, ParameterType.String);
         }
 
+        /// <summary>
+        /// レジスタ番号と子参照から <see cref="Lury.Compiling.IR.Parameter"/> オブジェクトを生成します。
+        /// </summary>
+        /// <param name="register">パラメータとして渡されるレジスタ番号。</param>
+        /// <param name="children">パラメータとして渡される子参照を表す文字列の配列。</param>
+        /// <returns>生成された <see cref="Lury.Compiling.IR.Parameter"/> オブジェクト。</returns>
         public static Parameter GetReference(int register, params string[] children)
         {
             return new Parameter(new Reference(register, children), ParameterType.Reference);
         }
 
+        /// <summary>
+        /// 変数名と子参照から <see cref="Lury.Compiling.IR.Parameter"/> オブジェクトを生成します。
+        /// </summary>
+        /// <param name="name">パラメータとして渡される変数名を表す文字列。</param>
+        /// <param name="children">パラメータとして渡される子参照を表す文字列の配列。</param>
+        /// <returns>生成された <see cref="Lury.Compiling.IR.Parameter"/> オブジェクト。</returns>
         public static Parameter GetReference(string name, params string[] children)
         {
             return new Parameter(new Reference(name, children), ParameterType.Reference);
         }
 
+        /// <summary>
+        /// ラベル名から <see cref="Lury.Compiling.IR.Parameter"/> オブジェクトを生成します。
+        /// </summary>
+        /// <param name="value">パラメータとして渡されるラベル名を表す文字列。</param>
+        /// <returns>生成された <see cref="Lury.Compiling.IR.Parameter"/> オブジェクト。</returns>
         public static Parameter GetLabel(string value)
         {
             if (string.IsNullOrWhiteSpace(value))
