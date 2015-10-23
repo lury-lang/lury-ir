ee47b3a85f55673024e70cd76bed090ef99b13f3
Tomona Nanase <nanase@users.noreply.github.com>
2015-10-24 01:56:04 +0900

    Add ParameterTest.cs

---
+++ UnitTest/ParameterTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Lury.Compiling.IR;
+using Lury.Compiling.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ParameterTest
+    {
+        [TestMethod]
+        public void NilTest()
+        {
+            var param = Parameter.Nil;
+
+            Assert.IsNull(param.Value);
+            Assert.AreEqual(ParameterType.Nil, param.Type);
+        }
+
+        [TestMethod]
+        public void TrueAndFalseTest()
+        {
+            var trueParam = Parameter.True;
+            var falseParam = Parameter.False;
+
+            Assert.IsTrue((bool)trueParam.Value);
+            Assert.AreEqual(ParameterType.Boolean, trueParam.Type);
+
+            Assert.IsFalse((bool)falseParam.Value);
+            Assert.AreEqual(ParameterType.Boolean, falseParam.Type);
+        }
+    }
+}
