32b480e950ea6325de2116584ea3943204d5d271
Tomona Nanase <nanase@users.noreply.github.com>
2015-10-26 15:56:50 +0900

    Update unittest ParameterTest

--- UnitTest/ParameterTest.cs
+++ UnitTest/ParameterTest.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using Lury.Compiling.IR;
-using Lury.Compiling.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest
@@ -106,6 +104,85 @@ namespace UnitTest
         public void GetStringError()
         {
             Parameter.GetString(null);
+        }
+
+        [TestMethod]
+        public void GetReferenceTest()
+        {
+            const int register = 3;
+            const string name = "fizzbuzz";
+            const string child1 = "foo";
+            const string child2 = "bar";
+
+            var registerParam1 = Parameter.GetReference(register, child1, child2);
+            var registerParam2 = Parameter.GetReference(register);
+            var nameParam1 = Parameter.GetReference(name, child1, child2);
+            var nameParam2 = Parameter.GetReference(name);
+
+            Assert.IsNotNull(registerParam1);
+            Assert.IsNotNull(registerParam2);
+            Assert.IsNotNull(nameParam1);
+            Assert.IsNotNull(nameParam2);
+
+            Assert.IsInstanceOfType(registerParam1.Value, typeof(Reference));
+            Assert.IsInstanceOfType(registerParam2.Value, typeof(Reference));
+            Assert.IsInstanceOfType(nameParam1.Value, typeof(Reference));
+            Assert.IsInstanceOfType(nameParam2.Value, typeof(Reference));
+
+            Assert.IsTrue(((Reference)registerParam1.Value).IsRegister);
+            Assert.IsTrue(((Reference)registerParam2.Value).IsRegister);
+            Assert.IsFalse(((Reference)nameParam1.Value).IsRegister);
+            Assert.IsFalse(((Reference)nameParam2.Value).IsRegister);
+
+            Assert.AreEqual(register, ((Reference)registerParam1.Value).Register);
+            Assert.AreEqual(register, ((Reference)registerParam2.Value).Register);
+            Assert.AreEqual(name, ((Reference)nameParam1.Value).Name);
+            Assert.AreEqual(name, ((Reference)nameParam2.Value).Name);
+
+            Assert.IsTrue(((Reference)registerParam1.Value).HasChildren);
+            Assert.IsFalse(((Reference)registerParam2.Value).HasChildren);
+            Assert.IsTrue(((Reference)nameParam1.Value).HasChildren);
+            Assert.IsFalse(((Reference)nameParam2.Value).HasChildren);
+
+            Assert.AreEqual(register, ((Reference)registerParam1.Value).Register);
+            Assert.AreEqual(register, ((Reference)registerParam2.Value).Register);
+            Assert.AreEqual(name, ((Reference)nameParam1.Value).Name);
+            Assert.AreEqual(name, ((Reference)nameParam2.Value).Name);
+
+            CollectionAssert.AreEquivalent(new[] { child1, child2 }, ((Reference)registerParam1.Value).Children.ToArray());
+            CollectionAssert.AreEquivalent(new[] { child1, child2 }, ((Reference)nameParam1.Value).Children.ToArray());
+            
+            Assert.AreEqual(ParameterType.Reference, registerParam1.Type);
+            Assert.AreEqual(ParameterType.Reference, registerParam2.Type);
+            Assert.AreEqual(ParameterType.Reference, nameParam1.Type);
+            Assert.AreEqual(ParameterType.Reference, nameParam2.Type);
+        }
+
+        [TestMethod]
+        public void GetLabelTest()
+        {
+            const string value = "Test String";
+
+            var param = Parameter.GetLabel(value);
+
+            Assert.IsNotNull(param);
+            Assert.IsInstanceOfType(param.Value, typeof(string));
+            Assert.AreEqual(value, param.Value);
+            Assert.AreEqual(ParameterType.Label, param.Type);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetLabelError1()
+        {
+            Parameter.GetLabel(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetLabelError2()
+        {
+            Parameter.GetLabel(string.Empty);
         }
     }
 }
