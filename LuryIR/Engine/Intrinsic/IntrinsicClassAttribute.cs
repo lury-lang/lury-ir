df3fa39a0c13dc98415dd6ab7ec5487bac3fbc74
Tomona Nanase <nanase@users.noreply.github.com>
2016-02-24 00:01:10 +0900

    add: IntrinsicClassAttribute.csを追加

---
+++ LuryIR/Engine/Intrinsic/IntrinsicClassAttribute.cs
@@ -0,0 +1,61 @@
+﻿//
+// IntrinsicClassAttribute.cs
+//
+// Author:
+//       Tomona Nanase <nanase@users.noreply.github.com>
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 Tomona Nanase
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+
+namespace Lury.Engine.Intrinsic
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    sealed class IntrinsicClassAttribute : Attribute
+    {
+        #region -- Private Fields --
+        
+        private readonly string fullname;
+        private readonly string typename;
+
+        #endregion
+
+        #region -- Public Propeties --
+
+        public string FullName => this.fullname;
+
+        public string TypeName => this.typename;
+
+        #endregion
+
+        #region -- Constructors --
+
+        public IntrinsicClassAttribute(string fullname, string typename)
+        {
+            this.fullname = fullname;
+            this.typename = typename;
+        }
+
+        #endregion
+    }
+}
